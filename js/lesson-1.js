73ab0ee089db2e0d8c24d5992dd4197f72fc4618
Mykyta_Rymar <mykitarymar@gmail.com>
2025-05-18 20:33:15 +0200

    ftyju

---
+++ js/lesson-1.js
@@ -0,0 +1,89 @@
+// task 1
+
+// const number = +prompt("Enter number");
+
+// if(number === 10) {
+//      alert('Вірно');
+// } else {
+//      alert('Невірно');
+// }
+
+// task 2
+
+// const min = Math.floor(Math.random() * (59 - 0) + 0);
+
+// if (min < 15) {
+//      alert(`${min} входить в першу чверть`);
+// } else if (min < 30) {
+//      alert(`${min} входить в другу чверть`);
+// } else if (min < 45) {
+//      alert(`${min} входить в третю чверть`);
+// } else {
+//      alert (`${min} входить в четверту чверть`);
+// }
+
+// таск 3
+
+// const num = +prompt('Enter number');
+// let result;
+
+// switch (num) {
+//      case 1:
+//           result = "зима";
+//           break;
+//      case 2:
+//           result = "весна";
+//           break;
+//      case 3:
+//           result = "літо";
+//           break;
+//      case 4:
+//           result = "осінь";
+//           break;
+//      default:
+//           result = "Вибачте, але ви маєте ввести значення від 1 до 4 включно";
+// }
+
+// console.log(result);
+
+// таск 5
+
+// const log = "Адмін";
+// const pass = "Я головний";
+
+// const login = prompt("Введіть логін");
+
+// if (login === log) {
+//   const password = prompt("Введіть пароль");
+//   if (password === pass) {
+//     alert("Добрий день!");
+//   } else {
+//     alert("Невірний пароль");
+//   }
+// } else if (login === null || login === "") {
+//   alert("Скасовано");
+// } else {
+//   alert("Я вас не знаю");
+// }
+
+
+// ТАСК 7
+
+// function getNumbers(min, max) {
+//   let sum = 0;
+//   for (let i = max; i >= min; i--) {
+//     if (i % 2 === 0) {
+//       sum += i;
+//     }
+//     console.log(i);
+//   }
+//   return sum;
+// }
+
+// console.log(getNumbers(4, 12));
+
+// task 10
+
+function fizzBuzz(num) {
+  
+}
